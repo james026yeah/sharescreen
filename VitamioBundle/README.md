4b13a5ae1b8f2a80e3ba85943d9c9f6c9b9e1996
root <root@james026-ThinkPad-T420.(none)>
2012-11-22 21:24:25 +0800

    add new files

--- VitamioBundle/README.md
+++
@@ -1,4 +0,0 @@
-Vitamio
-=======
-
-Vitamio for Android
--- aShareNetwork/gen/com/archermind/ashare/network/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.archermind.ashare.network;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
